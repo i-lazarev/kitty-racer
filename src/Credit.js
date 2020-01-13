d10dc66f759afe7fc3eca9ce9a5c33eaf0c443f9
i-lazarev <vanealazarev@gmail.com>
2020-01-13 13:57:44 +0100

    minor changes

--- src/Credit.js
+++ src/Credit.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {Route, NavLink} from "react-router-dom"
 
-export default function Credit() {
+export default function Credit({match}) {
     return (
         <div>
-            Credit
+            Who you want to credit?
+            <ul>
+                <li>
+                    <NavLink activeClassName="active" to={`${match.url}/React`}>React</NavLink>
+                </li>
+                <li>
+                <NavLink activeClassName="active" to={`${match.url}/ReactRouter`}>React Router</NavLink>
+                </li>
+            </ul>
+            <Route path={`${match.url}/:whoCredit`} component={Topic}></Route>
+            <Route exact render={()=><h3>Select something</h3>}/>
         </div>
     )
+}
+
+const Topic = ({match}) =>{
+    return <div>
+<h3>Topic selected {match.params.whoCredit}</h3>
+    </div>
 }
