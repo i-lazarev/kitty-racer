64cddda075dd23d2a0460ddc94b3f91ee3227aff
i-lazarev <vanealazarev@gmail.com>
2020-01-13 13:28:53 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// Libraries
+import {
+  BrowserRouter as Router,
+  Switch, 
+   NavLink, 
+   Route} from "react-router-dom"
+//Components
+import Home from "./Home"
+import About from "./About"
+import Credit from "./Credit"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Router>
+        <div>
+          <ul>
+            <li><NavLink exact activeClassName="active" to="/">Home</NavLink></li>
+            <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
+            <li><NavLink activeClassName="active" to="/credit">Credit</NavLink></li>
+            <li><NavLink activeClassName="active" to="/Does not exist">Nope</NavLink></li>
+          </ul>
+        </div>
+        
+          <Switch>
+            <Route exact path="/"component ={Home}/>          
+            <Route path="/about"component={About}/>
+            <Route path="/credit" component={Credit}/>
+            <Route path="*" render={()=><div>404</div>}/>
+          </Switch>
+
+        </Router>
+
       </header>
     </div>
   );
